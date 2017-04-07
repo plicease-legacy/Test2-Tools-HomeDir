df90c9cfbe090ccd52ea5ee61bc21b359f9ba755
Graham Ollis <plicease@cpan.org>
2017-04-07 11:49:55 -0400

    add some basic mocking

--- lib/Test2/Tools/HomeDir.pm
+++ lib/Test2/Tools/HomeDir.pm
@@ -2,10 +2,16 @@ package Test2::Tools::HomeDir;
 
 use strict;
 use warnings;
-use Path::Tiny ();
 use base qw( Exporter );
+use File::Temp qw( tempdir );
+use File::Spec;
+use constant _platform_env => [
+  $^O =~ /^(MSWin32|cygwin)$/ ? qw( USERPROFILE HOMEDRIVE HOMEPATH HOME ) : qw( HOME )
+];
 
-our @EXPORT = qw( real_home );
+die "Please contact author for support" if $^O eq 'msys';
+
+our @EXPORT = qw( real_home scope_home mock_home );
 
 # ABSTRACT: Test with a temporary faux home directory
 # VERSION
@@ -19,6 +25,53 @@ our @EXPORT = qw( real_home );
 TODO
 
 =head1 FUNCTIONS
+
+=head2 scope_home
+
+=cut
+
+our $home;
+
+sub scope_home (&)
+{
+  my($code) = @_;
+  local $ENV{HOME} = $ENV{HOME};
+  local $home = $home;
+  $code->();
+}
+
+=head2 mock_home
+
+ mock_home;
+
+Creates a new mock home directory.  Sets appropriate environment variables for the platform
+that you are running on.
+
+=cut
+
+sub mock_home
+{
+  $home = File::Temp->newdir;
+  
+  if($^O eq 'MSWin32')
+  {
+    $ENV{USERPROFILE} = "$home";
+    ($ENV{HOMEDRIVE}, $ENV{HOMEPATH}) = File::Spec->splitpath("$home");
+    $ENV{HOME} = "$home" if defined $ENV{HOME};
+  }
+  elsif($^O eq 'cygwin')
+  {
+    require File::Spec::Win32;
+    ($ENV{HOMEDRIVE}, $ENV{HOMEPATH}) = File::Spec->splitpath(Cygwin::posix_to_win_path("$home"));
+    $ENV{HOME} = "$home";
+  }
+  else
+  {
+    $ENV{HOME} = "$home";
+  }
+  
+  return;
+}
 
 =head2 real_home
 
@@ -36,6 +89,16 @@ sub real_home
   
   die "unable to determine real home directory";
 }
+
+*CORE::GLOBAL::getpwuid = sub {
+
+  return CORE::getpwuid(@_) unless $home;
+
+  my @list = getpwuid(@_);
+  $list[7] = "$home";
+  @list;
+
+};
 
 =head1 SEE ALSO
 
--- t/test2_tools_homedir.t
+++ t/test2_tools_homedir.t
@@ -10,4 +10,36 @@ subtest 'real home directory' => sub {
 
 };
 
+sub note_locations
+{
+  my($prefix) = @_;
+  note "$prefix: $_=@{[ defined $ENV{$_} ? $ENV{$_} : 'undef' ]}" for qw( HOME USERPROFILE USERDRIVE USERPATH );
+  note "$prefix: \$Test2::Tools::HomeDir::home=@{[ defined $Test2::Tools::HomeDir::home ? $Test2::Tools::HomeDir::home : 'undef' ]}";
+  
+}
+
+subtest 'mock_home' => sub {
+
+  my $mock_home;
+  my $env_name = $^O eq 'MSWin32' ? 'USERPROFILE' : 'HOME';
+
+  scope_home {
+    mock_home;
+
+    $mock_home = $ENV{$env_name};
+    
+    ok -d $mock_home, "mock home is a directory";
+    
+    note_locations('IN  SCOPE');
+    
+  };
+
+  my $real_home = $ENV{$env_name};
+
+  isnt $real_home, $mock_home, 'mock home is different from real home';
+
+  note_locations('OUT SCOPE');
+
+};
+
 done_testing;
